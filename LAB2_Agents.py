132ba27ce4624a476867c72f04adb602c13b31a9
VladimirWayOne <rekin.vladimir@yandex.ru>
2020-11-29 19:01:31 +0300

    update class Server (TODO: TypeError in MobilRobot)

--- LAB2_Agents.py
+++ LAB2_Agents.py
@@ -5,21 +5,23 @@ from tqdm import tqdm
 class Agent:
     """Единый класс Агента"""
     def __init__(self, name):
-        print('Агент ' + name + ' готов')
+        self.name = name
+        #print('Агент ' + self.name + ' готов')
 
 
 class Storage:
     """Класс Склада"""
     def __init__(self, maximum_things=10):
         self.maximum = maximum_things       # размер хранилища
-        print('Допустимая нагружемость: ', self.maximum)
+        # print('Допустимая нагружемость: ', self.maximum)
 
     _current = 0
     _storage = []
 
     def Accept(self, thing):
         """Функция приемки на складе"""
-
+        isReadyToAccept = True  #готовность к приемке
+        isReadyToIssue = False  #готовность к выдаче
         if self._current < self.maximum:
             self._current += 1
             self._storage.append(thing)
@@ -36,6 +38,7 @@ class Storage:
             if thing in self._storage:   # есть ли запрашиваемая вещь на складе
                 print('Процесс выдачи ', thing)
                 self._storage.remove(thing)
+                # return thing
             else:
                 print(thing, ': данного предмета/вещи нет')
 
@@ -77,6 +80,7 @@ class CuttingMachine:
         self.__isReadyForAccept = True      # готовность приемки
         self.__isReadyForCutting = False     # готовность резки
         self.__isReadyForIssue = False      # готовность выдачи
+        self.detail = None                  # текущая деталь в станке
 
     def Accept(self, detail):
         """Функция приемки детали"""
@@ -96,13 +100,14 @@ class CuttingMachine:
         else:
             print('Станок не готов к резке')
 
-    def Issue(self):
+    def Issue(self, detail):
         """Функция выдачи"""
         if self.__isReadyForIssue:
-            print('Выдача')
+            print('Выдача', detail)
             self.__isReadyForIssue = False
             print('Станок пуст и готов к приемке')
             self.__isReadyForAccept = True
+            # return detail
         else:
             print('Станок не готов к выдаче')
 
@@ -212,7 +217,7 @@ class Container:
     """Тара для заготовок и готовых изделий"""
     def __init__(self, maximum=10):
         self.maximum = maximum      # Максимальное количество содержимого в таре (по умолчанию 10)
-        self.isReadyToAccept = False
+        self.isReadyToAccept = True
         self.__isReadyToTransport = False
         self.content = []
         self.isReadyToIssue = False
@@ -279,10 +284,98 @@ class Server:
     TODO Сделать сервер, содержащий все маркеры готовности агентов
     попозже
     """
+    def __init__(self, Agent, Storage, MobileRobot, CuttingMachine, StationaryRobot, Machine, Container):
+        self.Agent = Agent
+        self.Storage = Storage
+        self.MobileRobot = MobileRobot
+        self.CuttingMachine = CuttingMachine
+        self.StationaryRobot = StationaryRobot
+        self.Machine = Machine
+        self.Container = Container
+
+    def P1(self, detail):   # Приемка на складе
+        if self.Storage.isReadyToAccept:
+            self.Storage.Accept(detail)
+        else:
+            print('Error!')
+
+    def P2(self, detail):
+        if self.Storage.isReadyToIssue:
+            return self.Storage.Issue(detail)
+        else:
+            print('Error!')
+
+    def P3(self, what, from_where, to_where):
+        self.MobileRobot(what, from_where, to_where)
+
+    def P4(self, detail):
+        self.CuttingMachine.Accept(detail)
+
+    def P5(self, detail):
+        self.CuttingMachine.Cutting(detail)
+
+    def P6(self, detail):
+        self.CuttingMachine.Issue(detail)
+
+    def P7(self):
+        self.Machine.Accept()
+
+    def P8(self):
+        self.Machine.Processing()
+
+    def P9(self):
+        self.Machine.Move()
+
+    def P10(self):
+        self.Machine.Issue()
+
+    def P11(self):
+        self.StationaryRobot.Load(True)
+
+    def P12(self):
+        self.StationaryRobot.Move(True, True)
+
+    def P13(self):
+        self.StationaryRobot.Removing(True)
+
+    def P14(self, detail):
+        self.Container.Accept(detail)
+
+    def P15(self,  what, from_where, to_where):
+        self.Container.Transporting()
+        self.MobileRobot(what, from_where, to_where)
+
+    def P16(self, detail):
+        self.Container.Issue(detail)
+
+    def StartProcCycle(self, detail):       # рабочий цикл для одной детали
+        self.P4(detail)
+        self.P5(detail)
+        self.P6(detail)
+        self.P14(detail)
+        self.P15(detail, 'Станок для резки', 'Токарно-фрезерные станки')
+        self.P16(detail)
+        pass
 
 
 if __name__ == "__main__":
+    details = ['det1', 'det2', 'det3']
+    agent = Agent('test')
+    storage = Storage()
+    mobilerobot = MobileRobot()
+    cuttingmachine = CuttingMachine()
+    statrobot = StationaryRobot()
+    machine = Machine()
+    contain = Container()
+    server = Server(agent, storage, mobilerobot, cuttingmachine, statrobot, machine, contain)
+    for det in details:
+        server.StartProcCycle(det)
+    """
+    #server = Server()
     robot = Agent('Жора')
+   # print(robot.name)
+    server = Server(robot)
+   # print(server.Agent.name)
     sklad = Storage(2)
     sklad.Accept('всякой хуйни')
     sklad.Accept('всякой хуйни_2')
@@ -297,3 +390,4 @@ if __name__ == "__main__":
     statrob.Load(False or True)
     statrob.Load(True)
    # help(CuttingMachine())
+   """
