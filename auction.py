3d28616a793b80cca40a3ddc5035b4aa2485ebed
VladimirWayOne <rekin.vladimir@yandex.ru>
2020-12-27 13:33:41 +0300

    auction update

--- auction.py
+++ auction.py
@@ -3,27 +3,33 @@ import time
 
 
 class Auctioneer:
-    def __init__(self, name, total_funds):
+    def __init__(self, name, total_funds, bet=5):
         self.name = name    # имя аукционера
         self.total_funds = total_funds  # всего средств
         self.current_rate = 0
         self.ready_to_bet = True
+        self.bet = bet
 
-    def take_bet(self):       # предложить стоимость
-        print("{} делает шаг в ".format(self.name), end=' ')
-        bet = random.randint(-1, 100)                      # float(input("{} делает шаг в ".format(self.name)))
-        print(bet)
-        if bet + self.current_rate > self.total_funds:
+    def take_bet(self, min_current):       # предложить стоимость
+        #print("{} делает ставку в ".format(self.name), end=' ')
+        #bet = random.randint(-1, 100)                      # float(input("{} делает шаг в ".format(self.name)))
+        #print(self.current_rate)
+        if self.bet + self.current_rate > self.total_funds:
             print("Недостаточно средств ({}), выбывает".format(self.name))
             self.ready_to_bet = False
-        elif bet <= -1:
+        elif self.bet <= -1:
             print("{} выбывает".format(self.name))
             self.ready_to_bet = False
-        elif bet == 0:
+        elif self.bet == 0:
             print("{} сохраняет ставку".format(self.name))
             self.ready_to_bet = False
         else:
-            self.current_rate += bet
+            while self.current_rate <= min_current:
+                self.current_rate += self.bet
+                #print(self.name, self.current_rate, min_current)
+            print("{} делает ставку в ".format(self.name), end=' ')
+            print(self.current_rate)
+
 
 
 class Lot:
@@ -51,26 +57,28 @@ class Auction:
         return self.current_winner
 
     def start_auction(self):
-        while [False for i in range(0, len(self.Participants))] != [x.ready_to_bet for x in self.Participants[:]]:
+        while [x.ready_to_bet for x in self.Participants[:]].count(True) > 1:
             for participant in self.Participants:
                 time.sleep(0.5)
                 if not participant.ready_to_bet:
                     continue
-                participant.take_bet()
-            self.current_winner = self.winner(self.Participants)
+                #print(self.winning_rate)
+                min_current = self.winning_rate
+                print("Текущая ставка:", min_current)
+                participant.take_bet(min_current)
+                self.current_winner = self.winner(self.Participants)
 
         return self.current_winner
 
 
 if __name__ == "__main__":
-    zakaz = Lot("Доставка до склада", 1)
-    auct1 = Auctioneer("Мобильный робот 1", 100)
-    auct2 = Auctioneer("Мобильный робот 2", 50)
-    auct3 = Auctioneer("Мобильный робот 3", 25)
+    zakaz = Lot("Производство", 1)
+    auct1 = Auctioneer("Заказ 1", 100, 20)
+    auct2 = Auctioneer("Заказ 2", 70, 10)
+    auct3 = Auctioneer("Заказ 3", 25)
     auctioneers = [auct1, auct2, auct3]
     auction = Auction(zakaz, auctioneers)
     a = [x.ready_to_bet for x in auction.Participants[:]]
     z = [False for i in range(0, len(auction.Participants))]
     winner = auction.start_auction()
     print(winner.name)
-    pass
